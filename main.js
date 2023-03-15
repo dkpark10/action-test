c8e4b0cbeb85009e37167281211bce17e7718a45
dkpark10 <dkpark10@naver.com>
2023-03-15 14:01:06 +0900

    feat: 슬랙봇 작성

---
+++ main.js
@@ -0,0 +1,41 @@
+const { slackBotToken } = require("./secret");
+const { WebClient } = require("@slack/web-api");
+
+/**
+ * @description 허가되지 않은 인증서 통과하기 위한 코드
+ */
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+(async function () {
+  const token = slackBotToken;
+
+  const web = new WebClient(token);
+
+  const baseURL = "http://api.shopping.zum.com:8080/data/front?slot=cjShoppingTabV1";
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const today = date.getDay();
+
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
+  const updatedTime = `${year}:${month}:${day} ${hour}:${minutes} ${week[today]}요일`;
+
+  try {
+    const channel = "#쇼핑로그";
+    const text = `${updatedTime} 테스트 텍스트`;
+
+    const result = await web.chat.postMessage({
+      channel,
+      text,
+    });
+
+    if (result?.ok && result.ok) {
+      console.log(result);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+})();
