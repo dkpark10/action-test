c9521aae11a3d21fa84c34ac88404a88e50f7649
dkpark10 <dkpark10@naver.com>
2024-04-25 14:28:30 +0900

    feat: 간단 서버 작성

--- main.js
+++ main.js
@@ -1,78 +1,78 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { WebClient } = require("@slack/web-api");
-const axios = require("axios");
+// /* eslint-disable @typescript-eslint/no-var-requires */
+// const { WebClient } = require("@slack/web-api");
+// const axios = require("axios");
 
-/**
- * @description 허가되지 않은 인증서 통과하기 위한 코드
- */
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+// /**
+//  * @description 허가되지 않은 인증서 통과하기 위한 코드
+//  */
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-(async function () {
-  let token;
-  const isLocalSave = process.argv[2] && process.argv[2] === "local";
-  if (isLocalSave) {
-    token = require("../../resources/static/keys").slackBotToken;
-  } else {
-    token = process.env.SLACK_BOT_TOKEN;
-  }
-  const web = new WebClient(token);
+// (async function () {
+//   let token;
+//   const isLocalSave = process.argv[2] && process.argv[2] === "local";
+//   if (isLocalSave) {
+//     token = require("../../resources/static/keys").slackBotToken;
+//   } else {
+//     token = process.env.SLACK_BOT_TOKEN;
+//   }
+//   const web = new WebClient(token);
 
-  const lessThan10 = (value) => (value < 10 ? `0${value}` : String(value));
+//   const lessThan10 = (value) => (value < 10 ? `0${value}` : String(value));
 
-  const cjShoppingUrl = "https://shopping.zum.com/cjone";
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = lessThan10(date.getMonth() + 1);
-  const day = lessThan10(date.getDate());
-  const hour = lessThan10(date.getHours());
-  const minutes = lessThan10(date.getMinutes());
-  const today = date.getDay();
+//   const cjShoppingUrl = "https://shopping.zum.com/cjone";
+//   const date = new Date();
+//   const year = date.getFullYear();
+//   const month = lessThan10(date.getMonth() + 1);
+//   const day = lessThan10(date.getDate());
+//   const hour = lessThan10(date.getHours());
+//   const minutes = lessThan10(date.getMinutes());
+//   const today = date.getDay();
 
-  const week = ["일", "월", "화", "수", "목", "금", "토"];
-  const updatedTime = `${year}:${month}:${day} ${hour}:${minutes} ${week[today]}요일`;
+//   const week = ["일", "월", "화", "수", "목", "금", "토"];
+//   const updatedTime = `${year}:${month}:${day} ${hour}:${minutes} ${week[today]}요일`;
 
-  try {
-    const { data } = await axios.get(cjShoppingUrl);
-    const regEx = /window.shoppingZum=(.*?)*\s/g;
+//   try {
+//     const { data } = await axios.get(cjShoppingUrl);
+//     const regEx = /window.shoppingZum=(.*?)*\s/g;
 
-    const flattendHtml = data
-      .split(" ")
-      .filter((item) => item !== "")
-      .map((item) => {
-        if (item.match(/\\n$/g)) {
-          return item.slice(0, -2);
-        }
-        return item;
-      })
-      .join("");
+//     const flattendHtml = data
+//       .split(" ")
+//       .filter((item) => item !== "")
+//       .map((item) => {
+//         if (item.match(/\\n$/g)) {
+//           return item.slice(0, -2);
+//         }
+//         return item;
+//       })
+//       .join("");
 
-    const beginIndex = "window.shoppingZum=".length;
-    const { cjoneMainData } = JSON.parse(flattendHtml.match(regEx)[0].slice(beginIndex));
+//     const beginIndex = "window.shoppingZum=".length;
+//     const { cjoneMainData } = JSON.parse(flattendHtml.match(regEx)[0].slice(beginIndex));
 
-    let cjDataMalls = {};
-    /**
-     * @description mall 속성만 추출한다.
-     */
-    Object.keys(cjoneMainData).forEach((key) => {
-      cjDataMalls = {
-        ...cjDataMalls,
-        [key]: cjoneMainData[key].map(({ mall }) => ({
-          ...mall,
-        })),
-      };
-    });
+//     let cjDataMalls = {};
+//     /**
+//      * @description mall 속성만 추출한다.
+//      */
+//     Object.keys(cjoneMainData).forEach((key) => {
+//       cjDataMalls = {
+//         ...cjDataMalls,
+//         [key]: cjoneMainData[key].map(({ mall }) => ({
+//           ...mall,
+//         })),
+//       };
+//     });
 
-    const channel = "C04TDGG3FNJ";
-    const text = `*${updatedTime}*\n` + "```\n" + JSON.stringify(cjDataMalls, null, 2) + "\n```";
-    const result = await web.chat.postMessage({
-      channel,
-      text,
-    });
+//     const channel = "C04TDGG3FNJ";
+//     const text = `*${updatedTime}*\n` + "```\n" + JSON.stringify(cjDataMalls, null, 2) + "\n```";
+//     const result = await web.chat.postMessage({
+//       channel,
+//       text,
+//     });
 
-    if (result?.ok && result.ok) {
-      console.log(result);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-})();
+//     if (result?.ok && result.ok) {
+//       console.log(result);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })();
--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('123');
+console.log('123', typeof window);
+
+fetch('http://localhost:8080/api/contents')
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.err(err);
+  });
