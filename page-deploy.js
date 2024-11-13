30713e7cb1441fa23e3c0f0f2555f65509b41005
dkpark10 <dkpark10@naver.com>
2024-11-13 10:26:41 +0900

    init commit

--- page-deploy.js
+++
@@ -1,8 +0,0 @@
-const ghPages = require('gh-pages');
-
-ghPages.publish('dist', {
-  repo: 'https://github.com/dkpark10/action-test.git',
-}, function (err) {
-  console.log(err);
-  throw new Error('page deploy error', err);
-});
